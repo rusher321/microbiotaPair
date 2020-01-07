238ec180b95a1ec20c78e3f3966f234c43b7bd53
任华慧 <renhuahui@genomics.cn>
2020-01-07 19:45:59 +0800

    init microbiotaPair package

---
+++ R/foo.R
@@ -0,0 +1,10 @@
+#' A brief introduction to this function.
+#' @param x1 explain what is x1
+#' @param x2 
+#' @return explain what is returned by this function.
+#' @export
+#' @examples foo()
+foo <- function(x1 = 2, x2 = 21) {
+  y <- x1 * x2
+  return(y)
+}
